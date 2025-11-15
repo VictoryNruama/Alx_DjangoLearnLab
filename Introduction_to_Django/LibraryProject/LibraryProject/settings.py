c343874321ce8efd84449224e8e969f11cc82cb5
VictoryNruama <victorynruama@gmail.com>
2025-11-15 23:26:54 +0100

    Refactor project structure: update secret key, remove unused files, and add missing configurations

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#x4-+lv7j2r916+ba61giz34j*d7j9pc(c)cifwmbkmited)kb'
+SECRET_KEY = 'django-insecure-9v3c^nle&4mq^c-j0msjs(r7b#^3402wxnnm11rwluuxu3t5@!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
